5d4030bed36e39f17d7e28f4aa0a0c628cc95869
marcelo almeida <jmfda00@gmail.com>
2019-12-02 22:39:47 -0200

    refactor users service

--- services/users.py
+++ services/users.py
@@ -1,9 +1,16 @@
 from os.path import dirname, realpath
-from flask import Flask
+# microframework for webapps
+from flask import Flask, request, Response
+# local data storage
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.exceptions import NotFound, ServiceUnavailable
+# data serialization
+from marshmallow import Schema, fields, post_load
+# to return HTTP status to incoming requests
+from http import HTTPStatus as http_status
+# read and dump as json data
 import json
-import requests
+# exception handling
+from werkzeug.exceptions import NotFound
 
 
 app = Flask(__name__)
@@ -21,13 +28,31 @@ class User(db.Model):
     def __repr__(self):
         return f"<User: {self.name}>"
 
+class UserSchema(Schema):
+    """ Defines how a user instance will be serialized"""
+    
+    class Meta:
+         """ Add meta attributes here """
+         ordered = True # The output will be ordered according to the order that the fields are defined in the class.
+
+    id = fields.Int(requeired=False)
+    name = fields.String()
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return User(**data)
+
+# instantiate the schema serializer
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
 @app.route("/", methods=['GET'])
 def hello():
-    return nice_json({
+    return json.dumps({
         "uri": "/",
         "subresource_uris": {
             "users": "/users",
-            "user": "/users/<username>",
+            "user": "/users/<user>",
             "bookings": "/users/<username>/bookings",
             "suggested": "/users/<username>/suggested"
         }
@@ -36,17 +61,53 @@ def hello():
 
 @app.route("/users", methods=['GET'])
 def users_list():
-    return nice_json(users)
+    """ Return all booking instances """
+    users = User.query.all()
+    serialized_objects = users_schema.dumps(users, sort_keys=True, indent=4)
+
+    return Response(
+        response=serialized_objects,
+        status=http_status.OK,
+        mimetype="application/json"
+    )
 
 
-@app.route("/users/<username>", methods=['GET'])
-def user_record(username):
-    if username not in users:
+@app.route("/users/<user>", methods=['GET'])
+def user_record(user):
+    user = User.query.get(user)
+
+    if not user:
         raise NotFound
 
-    return nice_json(users[username])
+    serialized_objects = user_schema.dumps(user, sort_keys=True, indent=4)
 
+    return Response(
+    response=serialized_objects,
+    status=http_status.OK,
+    mimetype="application/json"
+    )
 
+# Route for adding a new user
+@app.route("/users/new", methods=["POST"])
+def new_user():
+    """ POST a new user"""
+    new_user = None
+    try:
+        new_user = user_schema.loads(request.data)
+    except ValidationError as err:
+        pass
+        #TODO: send a exception  message
+    # save data:
+    db.session.add(new_user)
+    db.session.commit()
+
+    return Response(
+      response=user_schema.dumps(new_user, sort_keys=True, indent=4),
+      status=http_status.OK,
+      mimetype='application/json'
+      )
+
+# TODO: refactor this method
 @app.route("/users/<username>/bookings", methods=['GET'])
 def user_bookings(username):
     """
@@ -86,7 +147,7 @@ def user_bookings(username):
 
     return nice_json(result)
 
-
+# TODO: implement this
 @app.route("/users/<username>/suggested", methods=['GET'])
 def user_suggested(username):
     """
